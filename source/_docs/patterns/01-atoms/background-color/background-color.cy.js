cf17b2c9982ff3672b1862a494e19edfd7622773
wwu-gerardc <138731956+wwu-gerardc@users.noreply.github.com>
2024-02-15 16:12:28 -0800

    try adding retry params for cypress test

--- source/_docs/patterns/01-atoms/background-color/background-color.cy.js
+++ source/_docs/patterns/01-atoms/background-color/background-color.cy.js
@@ -6,7 +6,10 @@ describe('atom: background-color', () => {
   context("automated accessibility tests", () => {
     it("passes axe core accessibility tests", () => {
       cy.injectAxe()
-      cy.checkA11y('.theme-doc-markdown')
+      cy.checkA11y('.theme-doc-markdown', {
+        retries: 5,
+        interval: 100
+      })
     })
   })
 })
