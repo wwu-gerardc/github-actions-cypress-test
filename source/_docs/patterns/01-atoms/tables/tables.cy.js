bda4a25e23c72c92d13922897cc06b1e19f97224
wwu-gerardc <138731956+wwu-gerardc@users.noreply.github.com>
2024-02-15 16:25:35 -0800

    Update tables.cy.js

--- source/_docs/patterns/01-atoms/tables/tables.cy.js
+++ source/_docs/patterns/01-atoms/tables/tables.cy.js
@@ -6,7 +6,10 @@ describe("atoms: tables", () => {
   context("automated accessibility tests", () => {
     it("passes axe core accessibility tests", () => {
       cy.injectAxe()
-      cy.checkA11y(".theme-doc-markdown")
+       cy.checkA11y('.theme-doc-markdown', {
+        retries: 5,
+        interval: 100
+      })
     })
   })
 })
