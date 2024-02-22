685a58cd89be424311549378ded65d08f0c189ac
wwu-gerardc <138731956+wwu-gerardc@users.noreply.github.com>
2024-02-22 13:30:20 -0800

    Update background-color.cy.js

--- source/_docs/patterns/01-atoms/background-color/background-color.cy.js
+++ source/_docs/patterns/01-atoms/background-color/background-color.cy.js
@@ -1,15 +1,13 @@
 describe('atom: background-color', () => {
   beforeEach(() => {
     cy.visit('/patterns/atoms/background-color')
+    cy.get(".theme-doc-markdown").should("be.visible")
   })
 
   context("automated accessibility tests", () => {
     it("passes axe core accessibility tests", () => {
       cy.injectAxe()
-      cy.checkA11y('.theme-doc-markdown', {
-        retries: 5,
-        interval: 100
-      })
+      cy.checkA11y('.theme-doc-markdown')
     })
   })
 })
