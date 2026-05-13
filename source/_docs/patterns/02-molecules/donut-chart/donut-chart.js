617e2f63e5abefca101b9cbbdb49dbcb416ed0b8
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2026-05-13 11:04:08 -0700

    slices

--- source/_docs/patterns/02-molecules/donut-chart/donut-chart.js
+++ source/_docs/patterns/02-molecules/donut-chart/donut-chart.js
@@ -1,43 +1,53 @@
 if (
   (typeof context == "undefined" || (typeof context != "undefined" && context == document)) // makes it work in Drupal
   && typeof window !== "undefined" // makes it work in Node.js server side rendering
-  ) {
+) {
   const donut_chart_template = document.createElement("template");
   donut_chart_template.innerHTML = `
     <!--link rel="stylesheet" href="https://ashlar.blob.core.windows.net/ashlar-theme-files/css/components/donut-chart.css" /-->
-    <svg viewBox="0 0 31.83 31.83" role="img"> 
-    </svg>
+    <figure>
+      <figcaption>
+      </figcaption>
+      <ul>
+      </ul>
+    </figure>
   `;
-
+  
   class WWUDonutChart extends HTMLElement {
     constructor() {
       super();
     }
-
+    
     connectedCallback() {
       let element_exists = this.classList.contains("element-created");
       if (!element_exists) {
         this.appendChild(donut_chart_template.content.cloneNode(true));
         this.classList.add("element-created");
-        const data = this.querySelectorAll("li");
-        const svg = this.querySelector("svg");
+
+        this.querySelector("figcaption").innerText = this.getAttribute("label");
+
+        const data = this.querySelectorAll("wwu-chart-item");
+        const chart = this.querySelector("ul");
         let offset = 0;
 
-        svg.setAttribute("aria-label", "Donut chart of " + this.getAttribute("label"))
-        
         for (let i = 0; i < data.length; i++) {
-          const percent = data[i].innerText.substr(0, data[i].innerText.indexOf("%"));         
+          const li = document.createElement("li");
+          const pct = data[i].getAttribute("value");
+          const label = data[i].innerText;   
 
-          const slice = `
-          <circle r="15.915" cx="15.915" cy="15.915" 
-          style="stroke-dashoffset: ${offset}; 
-          stroke-dasharray: ${percent} 100;"/>`
-          svg.innerHTML += slice;
-          offset -= parseFloat(percent);
-        }      
+          li.innerHTML = `${label} - ${pct}%`;
+          li.style.setProperty('--percentage', `${pct}%`);
+          li.style.setProperty('--bg-color', `var(--chart-blue-${i})`);
+          li.style.setProperty('--offset', `${offset}`);
+
+          chart.append(li);
+          offset += parseInt(pct);
+        }    
       }
     }
   }
-
-  window.customElements.define("wwu-donut-chart", WWUDonutChart);
+  
+  if (!window.customElements.get('wwu-donut-chart')) {        
+    window.customElements.define("wwu-donut-chart", WWUDonutChart);    
+  }  
 }
